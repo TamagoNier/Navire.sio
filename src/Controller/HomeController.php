4970ac7dbafa5919cfdb4a210c0e484ced588d28
egor.gututui <egor.gututui@CP-SIO-W11-PG12.bts.sio>
2023-12-11 12:51:07 +0100

    creation de la nav bar et de l'icone : à ajouter : search footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/", name:'home_')]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'homepage')]
+    #[Route('/', name: 'homepage')]
     public function homepage(): Response
     {
-        return $this->render();
+        return $this->render('home/homepage.html.twig');
     }
 }
