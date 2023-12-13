1be72782abd58b6606539e92f5366843b48035c7
egor.gututui <egor.gututui@CP-SIO-W11-PG12.bts.sio>
2023-12-13 16:42:12 +0100

    creation des entity aisshiptype et navire

---
+++ src/Entity/Navire.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NavireRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NavireRepository::class)]
+class Navire
+{
+    #[Assert\Unique(fields:['imo', 'mmsi', 'indicatifAppel'])]
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    
+    #[ORM\Column(name:'imo', length: 7)]
+    #[Assert\Regex('[1-9][0-9]{6}', message:'Le numero IMO doit être unique et composé de 7 chiffre sans commencer par zero')]
+    private ?string $imo = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $mmsi = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $indicatifAppel = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $eta = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImo(): ?string
+    {
+        return $this->imo;
+    }
+
+    public function setImo(string $imo): static
+    {
+        $this->imo = $imo;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMmsi(): ?string
+    {
+        return $this->mmsi;
+    }
+
+    public function setMmsi(string $mmsi): static
+    {
+        $this->mmsi = $mmsi;
+
+        return $this;
+    }
+
+    public function getIndicatifAppel(): ?string
+    {
+        return $this->indicatifAppel;
+    }
+
+    public function setIndicatifAppel(string $indicatifAppel): static
+    {
+        $this->indicatifAppel = $indicatifAppel;
+
+        return $this;
+    }
+
+    public function getEta(): ?\DateTimeInterface
+    {
+        return $this->eta;
+    }
+
+    public function setEta(?\DateTimeInterface $eta): static
+    {
+        $this->eta = $eta;
+
+        return $this;
+    }
+}
