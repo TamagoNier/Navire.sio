1c569c14f0d940dd98e5bba217b26651c9933d9b
egor.gututui <egor.gututui@CP-SIO-W11-PG12.bts.sio>
2024-01-08 10:30:32 +0100

    creation navireType et fonction controller + debut twig

--- src/Controller/NavireController.php
+++ src/Controller/NavireController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\NavireRepository;
+use App\Form\NavireType;
 use App\Entity\Navire;
 
-#[Route('navire', name: 'navire')]
+#[Route('navire', name: 'navire_')]
 class NavireController extends AbstractController
 {
     #[Route('/voirtous', name:'voirtous')]
@@ -17,6 +19,24 @@ class NavireController extends AbstractController
         $navires = $repoNavire->findAll();
         return $this->render('navire/voirtous.html.twig', [
            'navires' =>$navires, 
+        ]);
+    }
+    #[Route('/editer/{id}', name:'editer')]
+    public function editer(int $id, Request $request, NavireRepository $repo, EntityManagerInterface $em): Response{
+        $navire = $repo->find($id);
+        
+        $form = $this->createForm(NavireType::class, $navire);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $navire = $form->getData();
+            $em->persist($navire);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+        
+        return $this->render('navire/edit.html.twig', [
+            'form'=>$form->createView(),
         ]);
     }
 }
---
+++ src/Form/NavireType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Navire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\EntityType;
+use App\Entity\AisShipType;
+use App\Entity\Pays;
+use App\Entity\Port;
+class NavireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('indicatifAppel', TextType::class)
+            ->add('indicatifAppel', TextType::class)
+            ->add('indicatifAppel', TextType::class)
+            ->add('indicatifAppel', TextType::class)
+            ->add('eta', DateTimeType::class,['widget'=>'single_text'])
+            ->add('largeur', IntegerType::class)
+            ->add('largeur', IntegerType::class)
+            ->add('tirantdeau', NumberType::class, array(
+                'scale'=>1
+            ))
+            ->add('aisShipType', EntityType::class, [
+                'class'=> AisShipType::class,
+                'choice_label'=>'libelle'
+                ])
+            ->add('pavillon', EntityType::class, [
+                'class'=>Pays::class,
+                'choice_label'=>'nom'
+                ])
+            ->add('destination', EntityYype::class, [
+                'class'=>Pays::class,
+                'choice_label'=>'nom'
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Navire::class,
+        ]);
+    }
+}
