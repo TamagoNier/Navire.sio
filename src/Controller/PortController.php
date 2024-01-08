81e039d16634e3c6457d34ecdaf5417cad57f72f
egor.gututui <egor.gututui@CP-SIO-W11-PG12.bts.sio>
2024-01-08 12:55:42 +0100

    creation port type et port controller

---
+++ src/Controller/PortController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortController extends AbstractController
+{
+    #[Route('/port', name: 'app_port')]
+    public function index(): Response
+    {
+        return $this->render('port/index.html.twig', [
+            'controller_name' => 'PortController',
+        ]);
+    }
+}
